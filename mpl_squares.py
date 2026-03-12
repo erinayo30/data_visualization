32a22a9ca865cdb4410624d0cb7c0e6f2baaae77
Joshua Farinmade <joshuafarinmade@gmail.com>
2026-03-12 22:26:25 +0100

    Day 12 plotting on the graph using packages

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,14 +1,27 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-squares = [1, 4, 9, 16, 25]
 
+sns.set()
+
+# plt.style.available
+# ['Solarize_Light2', '_classic_test_patch', '_mpl-gallery']
+
+x_values =[1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+# plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+# ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
+# ax.plot(input_values, squares, linewidth=3)
 
 # set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
 
+ax.scatter(2, 4)
 # set size of tick labels
 ax.tick_params(labelsize=14)
 
