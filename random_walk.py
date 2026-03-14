e7bd8726b11889621f9874e6e9b945445eee7b53
Joshua Farinmade <joshuafarinmade@gmail.com>
2026-03-14 13:37:09 +0100

    the commit is for day 13 and 14

---
+++ random_walk.py
@@ -0,0 +1,36 @@
+from random import choice
+
+class RandomWalk:
+    """A class to generate random walks"""
+
+    def __init__(self, num_points= 5000):
+        """Initialize attribute of a work"""
+        self.num_points = num_points
+
+        # all walk starks at (0,0)
+        self.x=[0]
+        self.y=[0]
+
+    def fill_walk(self):
+        """Calculate all points gain in the walk"""
+
+        while len(self.x_values)< self.num_points:
+            # Decide which direction to go and how far
+            x_direction= choice([1, -1])
+            x_distance = choice([0,1,2,3,4])
+            x_step = x_direction * y_distance
+
+            y_direction= choice([1, -1])
+            y_distance = choice([0,1,2,3,4])
+            y_step = x_direction * x_distance
+
+            # reject moves that go nowhere
+            if x_step == 0 and y_step ==0:
+                continue
+
+            # calculate the new position
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_value.append()
+            self.y_values.append()
